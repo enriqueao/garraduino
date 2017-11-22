48d297bb856ed2eb795dca19d12a2277a49098c2
Unknown <enriqueao96@gmail.com>
2017-11-21 18:44:35 -0600

    Configuración bluetooth

--- ledBluetooth/ledBluetooth.ino
+++ ledBluetooth/ledBluetooth.ino
@@ -9,26 +9,26 @@ SoftwareSerial bluetooth(6, 7); // RX, TX
 
 void setup()
 {
-// Start the hardware serial port
-Serial.begin(9600);
-pinMode(13, OUTPUT); // onboard LED
-digitalWrite(13, LOW); // switch OFF LED
-bluetooth.begin(9600);
+  // Start the hardware serial port
+  Serial.begin(9600);
+  pinMode(13, OUTPUT); // onboard LED
+  digitalWrite(13, LOW); // switch OFF LED
+  bluetooth.begin(9600);
 }
 
 void loop()
 {
-bluetooth.listen();
-// while there is data coming in, read it
-// and send to the hardware serial port:
-while (bluetooth.available() > 0) {
-char inByte = bluetooth.read();
-Serial.write(inByte);
-}
-
-// Read user input if available.
-if (Serial.available()){
-delay(10); // The DELAY!
-bluetooth.write(Serial.read());
-}
+  bluetooth.listen();
+  // while there is data coming in, read it
+  // and send to the hardware serial port:
+  while (bluetooth.available() > 0) {
+  char inByte = bluetooth.read();
+  Serial.write(inByte);
+  }
+  
+  // Read user input if available.
+  if (Serial.available()){
+  delay(10); // The DELAY!
+  bluetooth.write(Serial.read());
+  }
 }
