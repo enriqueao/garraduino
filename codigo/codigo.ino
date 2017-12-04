95159d33b80c800717788b4b2c2e3839b7e9f817
Jorge Maya <jorgitomaya1@gmail.com>
2017-12-04 17:11:33 -0600

    Zumbador añadido

--- codigo/codigo.ino
+++ codigo/codigo.ino
@@ -26,6 +26,9 @@ int posicionGarra = 25;
 int ledIzq = 1;
 int ledDer = 12;
 
+//Variable del zumbador
+int zum = 2;
+
 Psx Psx;                                                 
 
 unsigned int data = 0;                                 
@@ -48,6 +51,8 @@ void setup()
 
   pinMode(ledIzq, OUTPUT);
   pinMode(ledDer, OUTPUT);
+
+  pinMode(zum, OUTPUT);
 }
 
 void loop()
@@ -55,6 +60,9 @@ void loop()
   //Resetear Led's
   digitalWrite(ledIzq, LOW);
   digitalWrite(ledDer, LOW);
+
+  //Apagar zumbador
+  digitalWrite(zum,LOW);
   
   data = Psx.read();                                      
 
@@ -64,11 +72,24 @@ void loop()
     if(posicionGarra < 1){
       posicionGarra = 1;
     }
+    if(posicionGarra < 40 and posicionGarra <20){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(posicionGarra < 20){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(posicionGarra);
     servoGarra.write(posicionGarra);
     delay(20);
                                              
   }
+
+
+  
   if (data & psxR2){
     Serial.println("R2");
     base--;
@@ -76,20 +97,45 @@ void loop()
       base = 1;
       digitalWrite(ledIzq, HIGH);
     }
+    if(base < 50 and base > 250){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base < 25){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(base);
     servoBase.write(base);
     delay(20);                           
   }
+
+
+  
   if (data & psxL1){
     Serial.println("L1");
     posicionGarra++;
     if(posicionGarra > 64){
       posicionGarra = 63;
     }
+    if(posicionGarra > 40 and posicionGarra <55){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(posicionGarra > 55){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(posicionGarra);
     servoGarra.write(posicionGarra);
     delay(20);
   }
+
+  
   if (data & psxL2){
     Serial.println("L2");
     base++;
@@ -97,45 +143,106 @@ void loop()
       base = 180;
       digitalWrite(ledDer, HIGH);
     }
+    if(base > 130 and base <160){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base > 160){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(base);
     servoBase.write(base);
     delay(20);                           
   }
+
+
+  
   if (data & psxX){
     Serial.println("Left");
     der--;
     if(der < 1){
       der = 1;
     }
+    if(base < 40 and base > 20){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base < 20){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(der);
     servoDer.write(der);
     delay(20);                           
   }
+
+
+  
   if (data & psxTri){
     Serial.println("Tri");
     der++;
     if(der > 90){
       der = 90;
     }
+    if(base > 60 and base < 75){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base > 75){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(der);
     servoDer.write(der);
     delay(20);                             
   }
+
+
+  
   if (data & psxUp){
     Serial.println("UP");
     izq++;
      if(izq > 180){
       izq = 180;
     }
+    if(base > 130 and base <160){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base > 160){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
+    }
     Serial.println(izq);
     servoIzq.write(izq);
     delay(20);                        
   }
+
+  
   if (data & psxDown){
     Serial.println("Down");
     izq--;
     if(izq < 1){
       izq = 1;
+    }
+    if(base < 50 and base > 250){
+      digitalWrite(zum, HIGH);
+      delay(100);
+      digitalWrite(zum, LOW);
+    }
+    if(base < 25){
+      digitalWrite(zum, HIGH);
+      delay(50);
+      digitalWrite(zum, LOW);
     }
     Serial.println(izq);
     servoIzq.write(izq);
